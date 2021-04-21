2fce6323c631b1a2b1773bd96033ef8ea12a0373
DAVIDERINGO <dtnvh5@gmail.com>
2021-04-20 19:42:23 -0500

    Add project files.

---
+++ Semana3TagliacozzoNR/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Semana3TagliacozzoNR
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            string usuario = txtUser.Text;
+            string clave = txtPass.Text;
+            await Navigation.PushAsync(new VentanaDos(usuario,clave));
+            
+        }
+    }
+}
---
+++ Semana3TagliacozzoNR/VentanaDos.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Semana3TagliacozzoNR
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class VentanaDos : ContentPage
+    {
+        public VentanaDos(string usuario, string clave)
+        {
+            InitializeComponent();
+            lblUser.Text = "USUARIO: " + usuario;
+            lblPass.Text = "CLAVE: "+clave;
+        }
+    }
+}
